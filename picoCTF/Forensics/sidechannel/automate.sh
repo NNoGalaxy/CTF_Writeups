22c4811b88dce8616e1a8e4ed4a3bbf416d61fa7
nnogalaxy <58949613+NNoGalaxy@users.noreply.github.com>
2022-08-13 16:41:14 +0430

    Create automate.sh

---
+++ picoCTF/Forensics/sidechannel/automate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python3 exploit.py 00000000
+python3 exploit.py 10000000
+python3 exploit.py 20000000
+python3 exploit.py 30000000
+python3 exploit.py 40000000
+python3 exploit.py 50000000
+python3 exploit.py 60000000
+python3 exploit.py 70000000
+python3 exploit.py 80000000
+python3 exploit.py 90000000
